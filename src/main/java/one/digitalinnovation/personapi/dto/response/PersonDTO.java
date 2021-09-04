96367a978a7cab7a22d5340e8072e3719dd64d38
Lais Ziegler <laisziegler@gmail.com>
2021-09-04 07:41:59 -0300

    Updated dependencies

---
+++ src/main/java/one/digitalinnovation/personapi/dto/response/PersonDTO.java
@@ -0,0 +1,38 @@
+package one.digitalinnovation.personapi.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.boot.autoconfigure.web.servlet.ConditionalOnMissingFilterBean;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @NotNull
+    @CPF
+    private String cpf;
+
+    private String birthDate;
+
+    private List<PhoneDTO> phones;
+}
