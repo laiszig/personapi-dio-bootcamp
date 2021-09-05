a27e55789a5b9c590ad169008bae4ca7ce494737
Lais Ziegler <laisziegler@gmail.com>
2021-09-05 09:05:43 -0300

    Developed delete by ID

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -37,8 +37,14 @@ public class PersonController {
         return personService.listAll();
     }
 
-    @GetMapping("{/id}")
+    @GetMapping("/{id}")
     public PersonDTO findById (@PathVariable Long id) throws PersonNotFoundException {
         return personService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById (@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
     }
 }
